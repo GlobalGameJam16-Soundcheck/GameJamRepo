007d00afd04b48145f4a86f8bec1e2862279f832
Justice Frimpong <jfrimpon@andrew.cmu.edu>
2016-01-30 00:58:03 -0500

    almost fleshed out working barebones controller mechs

---
+++ GameJam/Assets/Scripts/pickUpBehavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickUpBehavior : MonoBehaviour {
+
+	private Vector3 origPos;
+	public GameObject bins; //empty gameobject containing all the bins
+	private float binDist;
+
+	// Use this for initialization
+	void Start () {
+		origPos = transform.position;
+		binDist = 2.5f;
+	}
+
+	public void released(){
+		//loop through all the bins, find the closest bin, if closest <= binDist, 
+		//translate pickup to that binPos and check if bin is correct bin, otherwise translate to origpos
+		Debug.Log("I was released!");
+		float mindist = Mathf.Infinity;
+		float distance;
+		Transform closestBin = transform;
+		foreach (Transform bin in bins.transform) {
+			distance = Vector3.Distance (transform.position, bin.position);
+			Debug.Log (distance);
+			if (distance < mindist) {
+				mindist = distance;
+				closestBin = bin;
+			}
+		}
+		if (mindist < binDist) {
+			Debug.Log ("snapped to bin!");
+			transform.position = closestBin.position;
+		} else {
+			Debug.Log ("snapped to origPos!");
+			transform.position = origPos;
+		}
+	}
+
+	//perhaps used for animations or jiggling or detecting if ghost nearby?
+//	// Update is called once per frame 
+//	void Update () {
+//	
+//	}
+}
---
+++ GameJam/Assets/Scripts/playerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	private bool holding;
+	private Vector3 mousePos;
+	private LayerMask pickups;
+
+	public GameObject grabber;
+	private GameObject pickup;
+
+	// Use this for initialization
+	void Start () {
+		holding = false;
+		mousePos = Vector3.zero;
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
+		pickups = 1 << LayerMask.NameToLayer ("pickup");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 camMousPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		mousePos = new Vector3 (camMousPos.x, camMousPos.y, -camMousPos.z);
+		setToMousePos (grabber);
+		Debug.DrawRay (transform.position, mousePos, Color.yellow);
+//		Debug.Log (mousePos.x + " " + mousePos.y);
+
+		checkForGrabbing ();
+	}
+
+	void checkForGrabbing(){
+		if (Input.GetMouseButtonDown(0) && !holding) {
+			RaycastHit2D hit;
+			if (hit = (Physics2D.Raycast (transform.position, mousePos, Mathf.Infinity, pickups))) {
+				pickup = hit.transform.gameObject;
+				setToMousePos (pickup);
+				holding = true;
+				Debug.Log ("pickup");
+			}
+		}
+		if (Input.GetMouseButton (0)) {
+			if (holding) {
+				setToMousePos (pickup);
+			}
+		} else {
+			release ();
+		}
+	}
+
+	void release(){
+		if (holding) {
+			//call pickup.release function that checks for any bins nearby and snap to nearest one, otherwise
+			//go back to original position
+			pickUpBehavior pickUpScript = pickup.GetComponent<pickUpBehavior>();
+			pickUpScript.released ();
+			holding = false;
+		}
+	}
+
+	void setToMousePos(GameObject obj){
+		obj.transform.position = new Vector3 (mousePos.x, mousePos.y, obj.transform.position.z);
+	}
+}
